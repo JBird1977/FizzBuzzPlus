b9b6dcb0873682dd6e4859550ea37f681708b1c1
Jaosn Westbrook <westbrook.jason@gmail.com>
2020-02-09 15:02:16 -0500

    Created first passing test

---
+++ src/main/java/FizzBuzzPlusApplication.java
@@ -0,0 +1,8 @@
+public class FizzBuzzPlusApplication
+{
+    public static int mainApplication(int inputNumber)
+    {
+        return 1;
+    }
+
+}
--- src/test/java/FizzBuzzPlusTest.java
+++ src/test/java/FizzBuzzPlusTest.java
@@ -1,3 +1,17 @@
-public class FizzBuzzPlusTest {
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzzPlusTest
+{
+
+    @Test
+    public void InputAOneExpectAOne()
+    {
+        int expected = 1;
+        int actual = FizzBuzzPlusApplication.mainApplication(1);
+        assertEquals(expected, actual);
+    }
+
 
 }
